66878a06da34e1410c35606ff0254a2e834e7434
Marina Antunes <78970016+antunes-marina@users.noreply.github.com>
2021-10-30 17:34:29 -0300

    modulos

---
+++ javascript/06-modulos/atividade/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Marina', 33));
+console.log(mostraCidade('Marina', 'Belo Horizonte'));
+console.log(mostraHobby('Marina', 'tocar violão'));
+
+
